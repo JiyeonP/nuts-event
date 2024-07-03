6e8f830e02bc40fbee797da10b4ed7fcf0434f0a
JiyeonP <q0o0p@snu.ac.kr>
2024-07-03 22:59:16 +0900

    feat: put login saving answers to google form

---
+++ src/components/Home.tsx
@@ -0,0 +1,32 @@
+import type React from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import "./Home.css";
+
+const Home: React.FC = () => {
+  const navigate = useNavigate();
+  const { type } = useParams<{ type?: string }>();
+
+  const handleRegisterClick = () => {
+    navigate(`/register${type ? `?type=${type}` : ""}`);
+  };
+
+  return (
+    <div className="home-container">
+      <img src="/logo.svg" alt="Logo" className="logo" />
+      <div className="description">
+        안녕하세요
+        <br /> 당신을 위한 디자이너 제품 커머스 NUTS <br />
+        궁금하다면
+      </div>
+      <button
+        type="button"
+        className="bt_register"
+        onClick={handleRegisterClick}
+      >
+        등록하기
+      </button>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/components/Register.tsx
@@ -0,0 +1,100 @@
+import type React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { useLocation } from "react-router-dom";
+
+const Register: React.FC = () => {
+  const [type, setType] = useState<string>("");
+  const [contact, setContact] = useState<string>("");
+  const [suggestions, setSuggestions] = useState<string>("");
+  const [contactError, setContactError] = useState<string>("");
+  const [suggestionsError, setSuggestionsError] = useState<string>("");
+
+  const location = useLocation();
+
+  useEffect(() => {
+    const query = new URLSearchParams(location.search);
+    const typeParam = query.get("type");
+    if (typeParam) {
+      setType(typeParam);
+    }
+  }, [location]);
+
+  const handleContactChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    if (value.length <= 20) {
+      setContact(value);
+      setContactError("");
+    } else {
+      setContactError("글자수가 20자를 초과했습니다.");
+    }
+  };
+
+  const handleSuggestionsChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    if (value.length <= 200) {
+      setSuggestions(value);
+      setSuggestionsError("");
+    } else {
+      setSuggestionsError("글자수가 200자를 초과했습니다.");
+    }
+  };
+
+  const handleSubmit = async () => {
+    try {
+      const form_url = process.env.REACT_APP_GOOGLE_FORM_URL as string;
+      const contact_entry = process.env.REACT_APP_CONTACT_ENTRY as string;
+      const suggestions_entry = process.env
+        .REACT_APP_SUGGESTIONS_ENTRY as string;
+      const type_entry = process.env.REACT_APP_TYPE_ENTRY as string;
+      const fullUrl = `${form_url}${
+        contact ? `&entry.${contact_entry}=${encodeURIComponent(contact)}` : ""
+      }${
+        suggestions
+          ? `&entry.${suggestions_entry}=${encodeURIComponent(suggestions)}`
+          : ""
+      }${
+        ["1", "2", "3", "4"].includes(type)
+          ? `&entry.${type_entry}=${type}`
+          : `&entry.${type_entry}=0`
+      }`;
+      console.log(fullUrl);
+      const response = await axios.get(fullUrl);
+      console.log(response.data);
+    } catch (error) {
+      console.error("Error submitting the form", error);
+    }
+  };
+
+  return (
+    <div className="register-container">
+      <h1 className="text-center">등록 페이지</h1>
+      <div>
+        <div>Contact</div>
+        <input
+          type="text"
+          placeholder="이메일 혹은 전화번호"
+          value={contact}
+          onChange={handleContactChange}
+        />
+        {contactError && <div style={{ color: "red" }}>{contactError}</div>}
+      </div>
+      <div>
+        <div>Suggestions</div>
+        <input
+          placeholder="당신이 원하는 디자이너 제품, 당신이 서비스에 등록하려는 이유, 당신의 오늘 저녁 메뉴, 당신의 하루 일과"
+          value={suggestions}
+          onChange={handleSuggestionsChange}
+        />
+        {suggestionsError && (
+          <div style={{ color: "red" }}>{suggestionsError}</div>
+        )}
+      </div>
+      <button type="submit" onClick={handleSubmit}>
+        제출하기
+      </button>
+    </div>
+  );
+};
+
+export default Register;
